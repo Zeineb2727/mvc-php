672d8e1cf725bc74a68181bc5365e7d9027f5f3a
Zeineb2727 <zeinebcosgun9@gmail.com>
2025-10-21 08:19:46 +0200

    Créez la vue et le contrôleur

---
+++ blog/post.php
@@ -0,0 +1,19 @@
+<?php
+$post = [
+    'title' => 'Unfaux titre.',
+    'french_creation_date' => '03/03/2022 à12h14min42s',
+    'content' => "Le faux contenu de mon billet.\nC'est fantastique!",
+];
+$comments = [
+    [
+        'author' => 'Un premier faux auteur',
+        'french_creation_date' => '03/03/2022 à12h15min42s',
+        'comment' => "Un faux commentaire.\n Le premier.",
+    ],
+    [
+        'author' => 'Un second faux auteur',
+        'french_creation_date' => '03/03/2022 à12h16min42s',
+        'comment' => "Un faux commentaire.\n Le second.",
+    ],
+];
+require('templates/post.php');
