c29c1a0e64aee4d09fcd2d4c4e67e9533168ea04
Zeineb2727 <zeinebcosgun9@gmail.com>
2025-10-21 08:24:44 +0200

    Dynamisez le contrôleur

--- blog/post.php
+++ blog/post.php
@@ -1,19 +1,13 @@
 <?php
-$post = [
-    'title' => 'Unfaux titre.',
-    'french_creation_date' => '03/03/2022 à12h14min42s',
-    'content' => "Le faux contenu de mon billet.\nC'est fantastique!",
-];
-$comments = [
-    [
-        'author' => 'Un premier faux auteur',
-        'french_creation_date' => '03/03/2022 à12h15min42s',
-        'comment' => "Un faux commentaire.\n Le premier.",
-    ],
-    [
-        'author' => 'Un second faux auteur',
-        'french_creation_date' => '03/03/2022 à12h16min42s',
-        'comment' => "Un faux commentaire.\n Le second.",
-    ],
-];
+require('src/model.php');
+if (isset($_GET['id']) && $_GET['id'] > 0) {
+    $identifier = $_GET['id'];
+} else {
+    echo 'Erreur :aucunidentifiantdebilletenvoyé';
+    die;
+}
+
+$post = getPost($identifier);
+$comments = getComments($identifier);
+
 require('templates/post.php');
--- blog/src/model.php
+++ blog/src/model.php
@@ -1,10 +1,11 @@
 <?php
 
-function getPosts() {
+function getPosts()
+{
     try {
         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-    } catch(Exception $e) {
-        die('Erreur : '.$e->getMessage());
+    } catch (Exception $e) {
+        die('Erreur : ' . $e->getMessage());
     }
 
     $statement = $database->query(
@@ -16,6 +17,7 @@ function getPosts() {
             'title' => $row['title'],
             'french_creation_date' => $row['french_creation_date'],
             'content' => $row['content'],
+            'identifier' => $row['id'],
         ];
 
         $posts[] = $post;
